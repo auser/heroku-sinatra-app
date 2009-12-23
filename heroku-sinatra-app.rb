b7004cf290a9c0f82c5d5de81a12166122ecdc03
Ari Lerner <arilerner@mac.com>
2009-12-23 04:27:01 -0800

    Added code section

--- heroku-sinatra-app.rb
+++ heroku-sinatra-app.rb
@@ -48,12 +48,6 @@ get '/' do
     <body>
       <div id="content">
         <h1>Sweet sweet bash content</h1>
-        <p>Man, bash is super powerful</p>
-        <h4>Oh shoot, this reminds me</h4>
-        <h1>I need to do the <a href="http://git-scm.com/">GIT</a> stuff</h1>
-        <pre><code>
-          curl -XPOST -d"{}" http://getbeehive.com:8080/apps/quick-balloon-08slv/deploy
-        </code></pre>
         <div id="footer">Footer</div>
       </div>
     </body>
